35d6b580b76296b658058a3ebba92070dd054c8e
abkrew <silvernoobxd@gmail.com>
2022-06-28 17:11:49 +0300

    add computer play choice

--- rockpaper.js
+++ rockpaper.js
@@ -1,54 +1,6 @@
-let playerScore = 0;
-let computerScore = 0;
 const choices = ["rock", "paper", "scissors"];
 function computerPlay() {
-  const randomChoice = choices[Math.floor(Math.random() * 3)];
-  return randomChoice;
+  return choices[Math.floor(Math.random() * 3)];
 }
-function playerPlay() {
-  let playerPick = prompt(
-    "Pick a piece from rock paper or scissors"
-  ).toLowerCase();
-  function playerCheck() {
-    if (choices.includes(playerPick)) {
-      return choices.includes(playerPick);
-    } else {
-      return false;
-    }
-  }
-  while (playerCheck() === false) {
-    playerPick = prompt("Pick a piece from rock , paper or scissors only!");
-  }
-  return playerPick;
-}
-let computerSelection;
-let playerSelection;
-function playRound() {
-  computerSelection = computerPlay();
-  playerSelection = playerPlay();
-  if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    console.log("You've won the round");
-    playerScore++;
-  } else if (playerSelection === computerSelection) {
-    console.log("It's a tie");
-  } else {
-    console.log("You've lost the round");
-    computerScore++;
-  }
-}
-
-for (let i = 0; i < 5; i++) {
-  playRound();
-  console.log(`You: ${playerSelection} , computer: ${computerSelection}`);
-}
-if (playerScore > computerScore) {
-  console.log(`You won the game by ${playerScore} to ${computerScore}`);
-} else if ((playerScore = computerScore)) {
-  console.log(`The game is a tie by ${playerScore} to ${computerScore}`);
-} else {
-  console.log(`You lost the game by ${computerScore} to ${playerScore}`);
-}
+computerPlay();
+console.log(computerPlay());
